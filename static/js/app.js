c2e0b779e84f747a0455e61daab1ddd9b099adb8
Jayce Spiva <jayce.spiva@gmail.com>
2024-07-09 02:23:37 -0700

    commit

--- static/js/app.js
+++ static/js/app.js
@@ -1,22 +1,27 @@
 // Build the metadata panel
 function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
+    
 
     // get the metadata field
+    let metadata = data.metadata;
 
 
     // Filter the metadata for the object with the desired sample number
-
+    let resultArray = metadata.filter(sambleObj => sambleObj.id == sample);
+    let result = resultArray[0];
 
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    let panel = d3.select('#sample-metadata');
 
     // Use `.html("") to clear any existing metadata
-
+    panel.html("");
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
-
+    Object.entries(result).forEach(([key, value]) => {
+      panel.append('p').text(`${key}: ${value}`);
+    })
   });
 }
 
@@ -25,19 +30,22 @@ function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the samples field
-
+    let samples = data.samples;
 
     // Filter the samples for the object with the desired sample number
-
+    let resultArray = samples.filter(sambleObj => sambleObj.id == sample);
+    let result = resultArray[0];
 
     // Get the otu_ids, otu_labels, and sample_values
-
+    let otu_ids = result.otu_ids;
+    let otu_labels = result.otu_labels;
+    let sample_values = result.sample_values;
 
     // Build a Bubble Chart
-
+    
 
     // Render the Bubble Chart
-
+    
 
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
 
